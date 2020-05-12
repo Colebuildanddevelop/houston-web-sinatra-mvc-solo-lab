1f2461a35a015e84fc89b4d3d8f3e12d303fabec
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-05-12 14:23:42 -0500

    puppies are fun

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < Sinatra::Base
     configure do
-        set :views, 'app/views'
-        set :method_override, true
-      end
+      set :views, 'app/views'
+      set :method_override, true
+    end
     
-      get '/' do
-        erb :welcome
-      end
+    get '/' do
+      erb :welcome
+    end
 
    
 end
--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,40 @@
 class PuppiesController < ApplicationController
+
+    get '/puppies/index' do 
+      @puppies = Puppy.all
+      erb :index
+    end
+
+    get '/puppies/new' do
+      erb :new 
+    end
+
+    get '/puppies/:id' do 
+      @puppy = Puppy.all.find { |p| p.id == params[:id].to_i }
+      erb :show 
+    end
+
+    post '/puppies/create' do
+      Puppy.create(params)
+      redirect '/puppies/index'
+    end
+
+    get '/puppies/:id/edit' do 
+      @puppy = Puppy.all.find { |p| p.id == params[:id].to_i }
+      erb :edit
+    end 
+
+    patch '/puppies/:id' do 
+      @puppy = Puppy.all.find { |p| p.id == params[:id].to_i }
+      @puppy.update({name: params[:name], breed: params[:breed]})
+      redirect '/puppies/index'
+    end
+
+    delete '/puppies/:id' do 
+      @puppy = Puppy.all.find { |p| p.id == params[:id].to_i }
+      @puppy.destroy
+      redirect '/puppies/index'
+    end
+
    
 end
